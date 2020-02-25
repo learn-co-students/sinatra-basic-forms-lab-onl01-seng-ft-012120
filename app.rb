f201c3894de1a3bbfe797e271a8f909ee697b7f3
reddevilcero <reddevil_cero@hotmail.com>
2020-02-25 19:00:30 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+
+    erb :create_puppy
+  end
+
+  post '/puppy' do
+    info = params.to_h.transform_keys!(&:to_sym)
+    @puppy = Puppy.new(info)
+  
+    erb :display_puppy
+  end
 end
